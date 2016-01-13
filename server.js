c69b469a546174d5a246386e75832f2a43f41890
MB Burch <marybethburch@gmail.com>
2016-01-13 12:25:08 -0700

    add socket, make es6 function format consistent

--- server.js
+++ server.js
@@ -18,9 +18,20 @@ var port = process.env.PORT || 3000;
 var server = http.createServer(app);
 
 if(!module.parent) {
-  server.listen(port, function () {
+  server.listen(port, () => {
    console.log('Listening on port ' + port + '.');
   });
 }
+
+const socketIo = require('socket.io');
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+  console.log('A user has connected.', io.engine.clientsCount);
+
+  socket.on('disconnect', () => {
+    console.log('A user has disconnected.', io.engine.clientsCount);
+  });
+});
 
 module.exports = server;
